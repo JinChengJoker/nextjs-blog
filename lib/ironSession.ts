4d4b44f976d51b436b6dc09c2b10e9882173ac7a
cheng <jinchengjoker@foxmail.com>
2021-06-16 11:14:52 +0800

    SECRET_COOKIE_PASSWORD

--- lib/ironSession.ts
+++ lib/ironSession.ts
@@ -10,7 +10,7 @@ export type NextIronHandler = (
 export const ironSession = (handle: NextIronHandler) =>
   withIronSession(handle, {
     cookieName: "next_blog",
-    password: "5569317f-7603-48f9-8518-e4f843b30425",
+    password: process.env.SECRET_COOKIE_PASSWORD as string,
     // if your localhost is served on http:// then disable the secure flag
     cookieOptions: {
       secure: process.env.NODE_ENV === "production",
